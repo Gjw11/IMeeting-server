23ab75518f4f08f271f0ac26504f5107e3d919de
gjw <358641273@qq.com>
2019-01-22 22:37:32 +0800

    new

--- src/main/java/com/IMeeting/controller/TimerTask.java
+++ src/main/java/com/IMeeting/controller/TimerTask.java
@@ -14,12 +14,12 @@ import java.util.Date;
 public class TimerTask {
     public final static long oneTime = 60 * 1000;
 
-    @Scheduled(fixedRate = oneTime)
-    public void startMeeting() throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        long nowTime = sdf.parse(sdf.format(new java.util.Date())).getTime();
-
-        System.out.println(new Date());
-
-    }
+//    @Scheduled(fixedRate = oneTime)
+//    public void startMeeting() throws ParseException {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        long nowTime = sdf.parse(sdf.format(new java.util.Date())).getTime();
+//
+//        System.out.println(new Date());
+//
+//    }
 }
