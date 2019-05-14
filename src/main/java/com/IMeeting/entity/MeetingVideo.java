11ce3cdbb4b12a4f205c9da978b69547ee0d6bbc
gjw <358641273@qq.com>
2019-05-14 21:38:28 +0800

    new

--- src/main/java/com/IMeeting/entity/MeetingVideo.java
+++ src/main/java/com/IMeeting/entity/MeetingVideo.java
@@ -12,11 +12,23 @@ public class MeetingVideo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String videoRoomName;
+    @Column(name = "user_id")
     private int createUserId;
     private String createTime;
     private int tenantId;
     private int status;
 
+    @OneToOne
+    @JoinColumn(name = "user_id",insertable = false,updatable = false,nullable = false)
+    private Userinfo userinfo;
+
+    public Userinfo getUserinfo() {
+        return userinfo;
+    }
+
+    public void setUserinfo(Userinfo userinfo) {
+        this.userinfo = userinfo;
+    }
 
     public int getTenantId() {
         return tenantId;
